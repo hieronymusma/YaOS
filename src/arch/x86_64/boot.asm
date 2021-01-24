1dccb083b153cc469ddc79da7db48807a285d866
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-01-24 10:28:46 +0100

    Finally switch to long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -10,9 +11,64 @@ start:
     call check_cpuid ; Error code: 1
     call check_long_mode ; Error code: 2
 
-    ; print OK to the screen
-    mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+    call set_up_page_tables
+    call enable_paging
+
+    ; load 64-bit gdt
+    lgdt [gdt64.pointer]
+
+    ; jump into 64 bit code
+    jmp gdt64.code:long_mode_start
+
+set_up_page_tables:
+    ; map first P4 entry to P3 table
+    mov eax, p3_table
+    or eax, 0b11 ; present + writable
+    mov [p4_table], eax
+
+    ; map first P3 entry to P2 table
+    mov eax, p2_table
+    or eax, 0b11 ; present + writable
+    mov [p3_table], eax
+
+    ;  map each P2 entry to a huge 2MiB page
+    mov ecx, 0 ; counter variable
+
+.map_p2_table:
+    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
+    mov eax, 0x200000               ; 2MiB
+    mul ecx                         ; start address of exc-th page
+    or eax, 0b10000011              ; present + writable + huge (2MiB size)
+    mov [p2_table + ecx * 8], eax   ; map ecx-th entry (each entry is 8 byte long)
+
+    inc ecx                         ; increment counter
+    cmp ecx, 512                    ; if counter == 512, the whole P2 table is mapped
+    jne .map_p2_table               ; else map the next entry
+
+    ret
+
+enable_paging:
+    ; load P4 to cr3 register
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
 
 ; Prints "ERR: " and the given error code
 ; parameter: error code (in ascii) in al
@@ -87,6 +143,22 @@ check_long_mode:
     jmp error
 
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
-    resb 64
+    resb 1024
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code: equ $ - gdt64
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1    ; gdt length
+    dq gdt64            ; actual pointer into gdt64
