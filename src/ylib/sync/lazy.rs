5eba8d32b694e7de13152c294479d7bb910053ed
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-02-22 22:20:58 +0100

    Minimize unsafe code in Lazy

--- src/ylib/sync/lazy.rs
+++ src/ylib/sync/lazy.rs
@@ -8,8 +8,6 @@ pub struct Lazy<T, F> {
     lock: SpinLock,
 }
 
-// pub struct LazyGuard<'a, T>(MutexGuard<'a, Option<T>>);
-
 unsafe impl<T, F> Sync for Lazy<T, F> {}
 unsafe impl<T, F> Send for Lazy<T, F> {}
 
@@ -27,27 +25,26 @@ impl<T, F: FnOnce() -> T> Lazy<T, F> {
     fn init(&self) {
         {
             self.lock.lock();
-            // SAFETY: Init is locked by spinlock.
-            unsafe {
-                let option_ptr = &self.value as *const Option<T>;
-                let mut_option_ptr = option_ptr as *mut Option<T>;
-                let value = &mut *mut_option_ptr;
 
-                if value.is_none() {
-                    let new_value = match self.init_function.take() {
-                        Some(f) => f(),
-                        None => panic!("Lazy instance has previously been poisoned"),
-                    };
-                    value.get_or_insert(new_value);
+            if self.value.is_none() {
+                let new_value = match self.init_function.take() {
+                    Some(f) => f(),
+                    None => panic!("Lazy instance has previously been poisoned"),
+                };
+                unsafe {
+                    // SAFETY: Init is locked by spinlock.
+                    let mut_option = &mut *(&self.value as *const Option<T> as *mut Option<T>);
+                    mut_option.get_or_insert(new_value);
                 }
             }
+
             self.lock.release();
         }
     }
 
     pub fn get_static_ref(&'static self) -> &'static T {
         self.init();
-        self.value.as_ref().unwrap()
+        self.deref()
     }
 }
 
@@ -55,6 +52,6 @@ impl<T, F: FnOnce() -> T> Deref for Lazy<T, F> {
     type Target = T;
     fn deref<'b>(&'b self) -> &'b T {
         self.init();
-        &*self.value.as_ref().unwrap()
+        self.value.as_ref().unwrap()
     }
 }
