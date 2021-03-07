0d36344373bdadf2a3d51d2734e5a5c4eb1d94ad
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-03-07 20:50:59 +0100

    Update setup_repository.sh to include all dependencies.

--- setup_repository.sh
+++ setup_repository.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt install -y nasm qemu-system-x86 xorriso
+sudo apt install -y nasm qemu-system-x86 xorriso make build-essential
 rustup component add rust-src
 ./scripts/add_rustfmt_hook.sh
